aebcee91e4f18cd17b70da87224b269260cee79a
sammerk23 <sammerkmarques@gmail.com>
2016-05-04 17:15:06 -0300

    adicionei os posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def post_list(request):
+    posts = Post.objects.all().order_by('created_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
